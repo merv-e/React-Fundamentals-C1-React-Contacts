1c9bf82ef57cacf4ced5e4680d6feabddbb9e70d
merv-e <34655454+merv-e@users.noreply.github.com>
2022-11-14 19:06:35 +0100

    app created

---
+++ src/components/ListContacts.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+function ListContacts({contacts}) {
+  return (
+    <ol className='contact-list'>
+        {contacts.map((person) => ( 
+          <li key={person.id} className="contact-list-item">
+            <div 
+                className='contact-avatar' 
+                style={{ 
+                  backgroundImage: `url(${person.avatarURL})`}}>
+            </div>
+            <div className='contact-details'>
+               <p>{person.name}</p>
+               <p>{person.handle}</p>
+            </div>
+            <button className='contact-remove'>Remove</button>
+          </li>))} 
+    </ol> 
+  )
+}
+
+export default ListContacts
