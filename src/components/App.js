e8b5ba3a738682d67616d5107c482eec722ef0e5
merv-e <34655454+merv-e@users.noreply.github.com>
2022-11-21 17:20:48 +0100

    exercise with useEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../css/App.css";
 import ListContacts from "./ListContacts";
+import * as ContactsAPI from "../utils/ContactsAPI";
+
 
 const App = () => { 
   const removeContact = (person) => {
-    setContacts(contacts.filter(c => c.id !== person.id))
+    ContactsAPI.remove(person);
+    setContacts(contacts.filter(c => c.id !== person.id));
   };
 
-  const [contacts, setContacts] = useState([
-    {
-      id: "karen",
-      name: "Karen Isgrigg",
-      handle: "@karen_isgrigg",
-      avatarURL: "http://localhost:5001/karen.jpg",
-    },
-    {
-      id: "richard",
-      name: "Richard Kalehoff",
-      handle: "@richardkalehoff",
-      avatarURL: "http://localhost:5001/richard.jpg",
-    },
-    {
-      id: "tyler",
-      name: "Tyler McGinnis",
-      handle: "@tylermcginnis",
-      avatarURL: "http://localhost:5001/tyler.jpg",
-    }
-  ]);
+  const [contacts, setContacts] = useState([]);
+  
+  useEffect(() => {
+    const getContacts = async() => {
+      const res = await ContactsAPI.getAll();
+      setContacts(res);
+    } 
+    getContacts();
+  }, []); 
+
+
   return (
   <div>
   <ListContacts contacts={contacts} onDeleteContact={removeContact}/> 
