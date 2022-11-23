09c7a6ea1b69080ff810bc0f22eea8cd90ab636e
merv-e <34655454+merv-e@users.noreply.github.com>
2022-11-23 16:41:15 +0100

    first part of the create-contact-form has been created. - 2 inputs, 1 btn

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
 
 const CreateContact = () => {
   return (
     <div>
-      CreateContact
+      <Link to="/"  className="close-create-contact">Close</Link>
+      <form className='create-contact-form'>
+        <ImageInput 
+          className="create-contact-avatar-input" 
+          name="avatar-URL" 
+          maxHeight={64} />
+          <div className='create-contact-details'>
+            <input type="text" name='name' placeholder='Name'/>
+            <input type="text" name='handle' placeholder='Handle'/>
+            <button>Add Contact</button>
+          </div>
+      </form>
     </div>
   )
 }
